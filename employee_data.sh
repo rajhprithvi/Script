972abf48f3778a551d8cbe3475b9e185aafd3127
rajhprithvi <rajhprithvi@gmail.com>
2021-12-18 18:31:27 +0530

    Update employee_data.sh

--- employee_data.sh
+++ employee_data.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-echo "Enter Employee Name:" 
-read Name
-echo "Enter Designation:"
-read Designation
-echo "Enter Age:"
-read Age
-echo "Enter Company:"
-read Company 
 
 echo "Enter Employee Name: $Name"
 echo "Enter Employee Designation: $Designation"
 
-echo "Enter Employee Age:$Age"
+echo "Enter Employee Age:$Age \n Enter Employee Company:$Company"
 
-echo "Enter Employee Company:$Company"
+
